698a0ac2d38161d17090cabd592c48b72cdda8a0
Mohanned <MoOoMeEe@hotmail.com>
2021-11-20 07:55:30 +0300

    Adding Sqlite data access class

---
+++ DataAcessLibrary/Databases/ISqliteDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DataAcessLibrary.Databases
+{
+    public interface ISqliteDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatment, U parameters, string connectionStringName);
+        void SaveData<T>(string sqlStatment, T parameters, string connectionStringName);
+    }
+}
---
+++ DataAcessLibrary/Databases/SqliteDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace DataAcessLibrary.Databases
+{
+    public class SqliteDataAccess : ISqliteDataAccess
+    {
+        private readonly IConfiguration config;
+
+        public SqliteDataAccess(IConfiguration config)
+        {
+            this.config = config;
+        }
+        public List<T> LoadData<T, U>(string sqlStatment,
+                                      U parameters,
+                                      string connectionStringName)
+        {
+            string connectionString = config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                List<T> rooms = connection.Query<T>(sqlStatment, parameters).ToList();
+                return rooms;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatment,
+                                T parameters,
+                                string connectionStringName)
+        {
+            string connectionString = config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Execute(sqlStatment, parameters);
+            }
+        }
+    }
+}
